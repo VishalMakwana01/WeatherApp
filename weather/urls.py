afcc678e2ec903c72932c9a78238b91c29d1bd02
VishalMakwana01 <vishal.makwana3@gmail.com>
2019-04-14 13:27:49 +0530

    New Features

--- weather/urls.py
+++ weather/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('',views.index)
+    path('', views.index, name='index_page'),
+    path('details/', views.details, name='details_page')
 ]
--- weather/views.py
+++ weather/views.py
@@ -8,8 +8,9 @@ def index(request):
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=52859687c521455b0ba8c720194b5b19"
 
     if request.method == 'POST':
-        form = CityForm(request.POST)
-        form.save()
+        if '_add' in request.POST:
+            form = CityForm(request.POST)
+            form.save()
 
     form = CityForm()
     cities = City.objects.all()
@@ -20,14 +21,35 @@ def index(request):
         city_weather = {
             'city': city.name,
             'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'],
             'icon': r['weather'][0]['icon'],
-            'temp_min': r['main']['temp_min'],
-            'temp_max': r['main']['temp_max'],
-            'humidity': r['main']['humidity']
         }
         weather_data.append(city_weather)
 
     context = {'weather_data': weather_data, 'form': form}
 
     return render(request, 'weather/weather.html', context)
+
+
+def details(request):
+
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=52859687c521455b0ba8c720194b5b19"
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+            'temp_min': r['main']['temp_min'],
+            'temp_max': r['main']['temp_max'],
+            'humidity': r['main']['humidity'],
+            'main': r['weather'][0]['main'],
+            'speed': r['wind']['speed']
+        }
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data}
+
+    return render(request, 'weather/details.html', context)
